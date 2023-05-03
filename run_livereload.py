4ba31da4d7ffaeb5ae81c35ca8858c139ce02c83
Piotr Esden-Tempski <piotr@esden.net>
2023-05-03 16:02:46 -0700

    livereload: Small usability improvements and documentation.

--- run_livereload.py
+++ run_livereload.py
@@ -1,6 +1,9 @@
 from livereload import Server, shell
+import os
 
 if __name__ == '__main__':
+    os.system('make html')
+    print("To stop the server press CTRL-C...")
     server = Server()
     server.watch('*.rst', shell('make html'), delay=1)
     server.watch('*/*.rst', shell('make html'), delay=1)
